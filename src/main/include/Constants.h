91b3b02880fa9cd5d1d5d69e5e9426fb5e4d47f3
jtebbi <jamesb0990@gmail.com>
2023-02-01 20:46:09 -0500

    Adds open loop tank drive with odometry

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -1,21 +1,28 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #pragma once
 
-/**
- * The Constants header provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants.  This should not be used for any other
- * purpose.
- *
- * It is generally a good idea to place constants into subsystem- or
- * command-specific namespaces within this header, which can then be used where
- * they are needed.
- */
+#include <numbers>
+
+#include <units/length.h>
+
+namespace DriveConstants {
+constexpr int kLeftFrontMotorId = 1;
+constexpr int kLeftRearMotorId = 2;
+constexpr int kRightFrontMotorId = 3;
+constexpr int kRightRearMotorId = 4;
+
+constexpr int kEncoderCPR = 2048;
+constexpr int kGearReduction = 6;
+constexpr auto kWheelDiameter = 8_in;
+constexpr auto kEncoderDistancePerPulse =
+    std::numbers::pi * kWheelDiameter / (double)(kEncoderCPR * kGearReduction);
+
+constexpr double kTalonRampRate =
+    0.5; // 0.5 seconds from neutral to full throttle.
+constexpr int kTalonTimeoutMs = 30;
+} // namespace DriveConstants
 
 namespace OperatorConstants {
-
-constexpr int kDriverControllerPort = 0;
-
-}  // namespace OperatorConstants
+constexpr int kLeftJoystickPort = 1;
+constexpr int kRightJoystickPort = 2;
+constexpr int kXboxControllerPort = 3;
+} // namespace OperatorConstants
